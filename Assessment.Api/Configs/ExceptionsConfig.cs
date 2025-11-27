88b58d17a2a26bab6a5a0163ea42b04d8eb54be4
170anton <117779437+170anton@users.noreply.github.com>
2025-11-27 15:46:24 +0100

    [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Implement Exception Handling in Assessment project (#3)
    * [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Implement Global Exception Handler
    
    * [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Refactor registration of api layer services into ApiServiceExtensions
    
    * [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Refactor GlobalExceptionHandler to Assessment.Common
    
    * [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Create Global exception handler and config
    
    * [c03c9bb4-2b7a-4726-adb8-f670e3a87724] - Create BaseException

---
+++ Assessment.Api/Configs/ExceptionsConfig.cs
@@ -0,0 +1,13 @@
+using Assessment.Infrastructure.Handlers;
+
+namespace Assessment.API.Configs;
+
+public static class ExceptionsConfig
+{
+    public static WebApplicationBuilder ConfigExceptionsHandling(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
+
+        return builder;
+    }
+}
--- Assessment.Api/Program.cs
+++ Assessment.Api/Program.cs
@@ -1,13 +1,17 @@
+
+using Assessment.API.Configs;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.ConfigExceptionsHandling();
+
 var app = builder.Build();
+
+app.UseExceptionHandler();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
