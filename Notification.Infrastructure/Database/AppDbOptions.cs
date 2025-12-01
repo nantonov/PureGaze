7d1d6fef7b85056c41f2c11d3312f3ce840af647
Anton Jakubau <170anton@gmail.com>
2025-12-01 16:00:51 +0100

    [828b57a1-5bb5-43f9-b6a5-b7b635d4a80b] - Add Notification DB config

---
+++ Notification.Infrastructure/Database/AppDbOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Notification.Infrastructure.Database;
+
+public class AppDbOptions
+{
+    public const string SectionName = "DatabaseOptions";
+    
+    public string ConnectionString { get; set; } = string.Empty;
+    public int MaxRetryCount { get; set; }
+    public int MaxRetryDelaySecond { get; set; }
+}
