08e0dadf78f05c075b9aebdcfc71a8412c95ffd4
170anton <117779437+170anton@users.noreply.github.com>
2025-12-04 11:13:59 +0100

    [5435869f-53c3-40b2-8653-ed94ad557f53] - Create entity configuration

---
+++ Assessment.Infrastructure/Database/Configuration/SubtopicConfiguration.cs
@@ -0,0 +1,27 @@
+using Assessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assessment.Infrastructure.Database.Configuration;
+
+public class SubtopicConfiguration : IEntityTypeConfiguration<Subtopic>
+{
+    public void Configure(EntityTypeBuilder<Subtopic> builder)
+    {
+        builder.ToTable("Subtopics");
+
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.Id).ValueGeneratedOnAdd();
+
+        builder.Property(o => o.Name)
+            .IsRequired()
+            .HasMaxLength(128);
+
+        builder.Property(o => o.TopicId)
+            .IsRequired();
+        builder.HasOne<Topic>()
+            .WithMany() 
+            .HasForeignKey(o => o.TopicId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
---
+++ Assessment.Infrastructure/Database/Configuration/TemplateConfiguration.cs
@@ -0,0 +1,23 @@
+using Assessment.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Assessment.Infrastructure.Database.Configuration;
+
+public class TemplateConfiguration : IEntityTypeConfiguration<Template>
+{
+    public void Configure(EntityTypeBuilder<Template> builder)
+    {
+        builder.ToTable("Templates");
+
+        builder.HasKey(o => o.Id);
+        builder.Property(o => o.Id).ValueGeneratedOnAdd();
+
+        builder.Property(o => o.CodeId)
+            .IsRequired();
+        builder.HasOne<Code>()
+            .WithMany() 
+            .HasForeignKey(o => o.CodeId)
+            .OnDelete(DeleteBehavior.Cascade);
+    }
+}
