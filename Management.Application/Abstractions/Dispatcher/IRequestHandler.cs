7c39d78be394f401a1081cc87c8a7fae9ffe9230
maksim-kez <maksim.kez@innowise.com>
2025-12-11 12:21:29 +0100

    migration added

---
+++ Management.Application/Abstractions/Dispatcher/IRequestHandler.cs
@@ -0,0 +1,11 @@
+﻿namespace Management.Application.Abstractions.Dispatcher;
+
+public interface IRequestHandler<in TRequest> where TRequest : IRequest
+{
+    Task Handle(TRequest request, CancellationToken ct);
+}
+
+public interface IRequestHandler<in TRequest, TResponse> where TRequest : IRequest<TResponse>
+{
+    Task<TResponse> Handle(TRequest request, CancellationToken ct);
+}
