f0a80b18f71279ebff9426efeeb7b7f3b33a367e
maksim-kez <maksim.kez@innowise.com>
2025-12-11 13:25:11 +0100

    changed to email

--- Common.DAL/Configurations/EmailConfiguration.cs
+++ Common.DAL/Configurations/EmailConfiguration.cs
@@ -4,15 +4,20 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace Common.DAL.Configurations;
 
-public class NotificationConfiguration : IEntityTypeConfiguration<Notification>
+public class EmailConfiguration : IEntityTypeConfiguration<Email>
 {
-    public void Configure(EntityTypeBuilder<Notification> builder)
+    public void Configure(EntityTypeBuilder<Email> builder)
     {
-        builder.ToTable("Notifications");
+        builder.ToTable("Emails");
         builder.HasKey(x => x.Id);
 
-        builder.Property(x => x.Title).HasMaxLength(200).IsRequired();
-        builder.Property(x => x.Message).IsRequired();
+        builder.Property(x => x.Subject).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Body).IsRequired();
+        builder.Property(x => x.To).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.From).HasMaxLength(200).IsRequired();
+        builder.Property(x => x.Cc).HasMaxLength(500);
+        builder.Property(x => x.Bcc).HasMaxLength(500);
+        
         builder.Property(x => x.ErrorMessage).HasMaxLength(500);
 
         builder.HasOne(x => x.Employee)
--- Common.Domain/Enums/NotificationStatus.cs
+++
@@ -1,9 +0,0 @@
-namespace Common.Domain.Enums;
-
-public enum NotificationStatus
-{
-    Pending = 1,
-    Sending = 2,
-    Sent = 3,
-    Failed = 4
-}
