a7e6d20ddabc756cb2128adba8ef998bdbca4076
maksim-kez <maksim.kez@innowise.com>
2025-12-15 12:14:06 +0100

    changed email, removed unnecessary fields

--- Common.Domain/Entities/Email.cs
+++ Common.Domain/Entities/Email.cs
@@ -4,16 +4,14 @@ namespace Common.Domain.Entities;
 
 public class Email : BaseEntity<Guid>
 {
-    public int EmployeeId { get; set; }
-    public virtual Employee? Employee { get; set; }
-
+    public Guid EmployeeId { get; set; }
     public string Subject { get; set; } = string.Empty;
     public string Body { get; set; } = string.Empty;
     
     public string To { get; set; } = string.Empty;
     public string From { get; set; } = string.Empty;
-    public string? Cc { get; set; }
-    public string? Bcc { get; set; }
+
+    public int RetryCount { get; set; }
 
     public EmailPriority Priority { get; set; }
     public EmailStatus Status { get; set; }
