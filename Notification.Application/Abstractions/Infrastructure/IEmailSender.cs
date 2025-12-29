4c09a2dfc84f04ef7dde78f2b879c00561454cd5
MaksimKez <156958833+MaksimKez@users.noreply.github.com>
2025-12-29 15:52:01 +0100

    [5495adb1-8813-4e6f-a342-12f9920d5365] - Configure SMTP

--- Notification.Application/Abstractions/Infrastructure/IEmailSender.cs
+++ Notification.Application/Abstractions/Infrastructure/IEmailSender.cs
@@ -4,5 +4,5 @@ namespace Notification.Application.Abstractions.Infrastructure;
 
 public interface IEmailSender
 {
-    Task<bool> SendAsync(Email email, CancellationToken cancellationToken = default);
+    Task SendAsync(Email email, CancellationToken cancellationToken = default);
 }
