742a04ec7a14e26434941aa86bbe7c21aad67d2b
MaksimKez <156958833+MaksimKez@users.noreply.github.com>
2025-12-29 11:51:25 +0100

    [30843f95-d1d1-4b9e-a256-0830882e1a17] - Created factory for emails in assessment project

---
+++ Assessment.Application/Abstractions/Infrastructure/IEmailFactory.cs
@@ -0,0 +1,9 @@
+﻿using Common.Domain.Entities;
+
+namespace Assessment.Application.Abstractions.Infrastructure;
+
+public interface IEmailFactory
+{
+    Email CreateEmailForM(string managerEmail, string employeeName, DateTime requestedDate);
+    Email CreateEmailForEmployee(string employeeEmail, DateTime requestedDate);
+}
