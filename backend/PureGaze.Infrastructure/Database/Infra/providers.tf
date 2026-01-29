cfb3ecdde4ba453a540b059c88fa6d35c19f037d
Danyil Danilian <daniliandan100500@gmail.com>
2026-01-29 16:17:22 +0100

    [44c2451b-bbd3-4990-aced-e4e35924b0b7] - update terraform file.

--- backend/PureGaze.Infrastructure/Database/Infra/providers.tf
+++ backend/PureGaze.Infrastructure/Database/Infra/providers.tf
@@ -5,7 +5,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
     http = {
       source  = "hashicorp/http"
@@ -16,6 +16,7 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.azure_subscription_id
 }
 
 data "http" "myip" {
