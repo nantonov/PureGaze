1e2d018d582b640061d77ab5752910230ae7b187
Dzmitry Buyalski <buyalskiy.dmitriy@gmail.com>
2026-02-09 21:54:43 +0100

    Update settings info

--- frontend/src/config/auth0.ts
+++ frontend/src/config/auth0.ts
@@ -3,7 +3,9 @@ import type { Auth0Config } from "../types/Auth0Config";
 export const auth0Config: Auth0Config = {
   domain: import.meta.env.VITE_AUTH0_DOMAIN,
   clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
-  cacheLocation: (import.meta.env.VITE_AUTH0_Cache_Location || "localstorage") as "memory" | "localstorage",
+  cacheLocation: (import.meta.env.VITE_AUTH0_Cache_Location || "localstorage") as
+    | "memory"
+    | "localstorage",
   authorizationParams: {
     redirect_uri: window.location.origin,
     audience: import.meta.env.VITE_AUTH0_AUDIENCE,
