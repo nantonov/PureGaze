bee2c88b27410fc2f3fb0a192c295ef112b010a6
Danyil Danilian <daniliandan100500@gmail.com>
2026-02-09 14:57:08 +0100

    chore: comment out `max_size_gb` in Terraform SQL configuration

--- backend/PureGaze.Infrastructure/Terraform/sql.tf
+++ backend/PureGaze.Infrastructure/Terraform/sql.tf
@@ -14,5 +14,5 @@ resource "azurerm_mssql_database" "db" {
   sku_name                    = var.db_sku_name
   # min_capacity                = var.db_min_capacity
   # auto_pause_delay_in_minutes = var.db_auto_pause_delay
-  max_size_gb                 = var.db_max_size_gb
+  # max_size_gb                 = var.db_max_size_gb
 }
