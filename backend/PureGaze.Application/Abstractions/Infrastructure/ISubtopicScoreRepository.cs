2a5eb489ee37e2d8e420acb7c1cdb663bdb31b72
Romfos <Romfos@tut.by>
2026-02-10 10:02:25 +0100

    code review

--- backend/PureGaze.Application/Abstractions/Infrastructure/ISubtopicScoreRepository.cs
+++ backend/PureGaze.Application/Abstractions/Infrastructure/ISubtopicScoreRepository.cs
@@ -6,6 +6,6 @@ public interface ISubtopicScoreRepository
 {
     Task AddAsync(SubtopicScore subtopicScore, CancellationToken ct = default);
     Task UpdateAsync(SubtopicScore subtopicScore, CancellationToken ct = default);
-    Task<SubtopicScore?> GetBySubtopicAndStageIdAsync(int SubtopicId, int StageId, CancellationToken ct = default);
+    Task<SubtopicScore?> GetBySubtopicAndStageIdAsync(int subtopicId, int stageId, CancellationToken ct = default);
     Task SaveChangesAsync(CancellationToken ct = default);
 }
--- backend/PureGaze.Infrastructure/Database/Repositories/SubtopicScoreRepository.cs
+++ backend/PureGaze.Infrastructure/Database/Repositories/SubtopicScoreRepository.cs
@@ -4,19 +4,19 @@ using PureGaze.Domain.Entities;
 
 namespace PureGaze.Infrastructure.Database.Repositories;
 
-public class SubtopicScoreRepository(AppDbContext context) 
+public class SubtopicScoreRepository(AppDbContext context)
     : ISubtopicScoreRepository
 {
     public async Task AddAsync(SubtopicScore subtopic, CancellationToken ct = default)
         => await context.SubtopicScores.AddAsync(subtopic, ct);
 
-    public async Task<SubtopicScore?> GetBySubtopicAndStageIdAsync(int SubtopicId, int StageId, CancellationToken ct = default)
+    public async Task<SubtopicScore?> GetBySubtopicAndStageIdAsync(int subtopicId, int stageId, CancellationToken ct = default)
         => await context.SubtopicScores
-            .FirstOrDefaultAsync(s => s.SubtopicId == SubtopicId && s.StageId == StageId, ct);
+            .FirstOrDefaultAsync(s => s.SubtopicId == subtopicId && s.StageId == stageId, ct);
 
     public async Task SaveChangesAsync(CancellationToken ct = default)
         => await context.SaveChangesAsync(ct);
 
-    public async Task UpdateAsync(SubtopicScore subtopicScore, CancellationToken ct = default) 
+    public async Task UpdateAsync(SubtopicScore subtopicScore, CancellationToken ct = default)
         => context.Update(subtopicScore);
 }
