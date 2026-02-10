7d7f2ce018909f466e25c012680ddf50e2dbc74b
Danyil Danilian <daniliandan100500@gmail.com>
2026-02-10 16:04:12 +0100

    chore: remove ping endpoints and related frontend logic, clean up debug logs

--- backend/PureGaze.API/Configurations/SwaggerConfig.cs
+++ backend/PureGaze.API/Configurations/SwaggerConfig.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.OpenApi;
-using Microsoft.OpenApi.Models;
 
 namespace PureGaze.API.Configurations;
 
@@ -18,10 +17,10 @@ public static class SwaggerConfig
                 Description = "JWT Authorization header using the Bearer scheme."
             });
 
-            // x.AddSecurityRequirement(document => new OpenApiSecurityRequirement
-            // {
-            //     [new OpenApiSecuritySchemeReference("bearer", document)] = []
-            // });
+            x.AddSecurityRequirement(document => new OpenApiSecurityRequirement
+            {
+                [new OpenApiSecuritySchemeReference("bearer", document)] = []
+            });
         });
     }
 
@@ -32,7 +31,7 @@ public static class SwaggerConfig
         
         app.UseSwagger(x =>
         {
-           //x.OpenApiVersion = OpenApiSpecVersion.OpenApi3_1;
+            x.OpenApiVersion = OpenApiSpecVersion.OpenApi3_1;
         });
         app.UseSwaggerUI();
     }
--- backend/PureGaze.API/Program.cs
+++ backend/PureGaze.API/Program.cs
@@ -33,8 +33,7 @@ app.UseCors(CorsOptions.PolicyName);
 app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapGet("/ping/public", () => "pong (public)").AllowAnonymous();
-app.MapGet("/ping/secure", () => "pong (secure)").RequireAuthorization();
+
 
 app.MapControllers();
 
