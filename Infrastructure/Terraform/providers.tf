f3c928d3475db7564fc1065e9d310108a03760b2
Danyil Danilian <daniliandan100500@gmail.com>
2026-02-17 15:31:32 +0100

    [9e1fcd0c-f495-4fc9-b50e-a88f54a1675a] - Moved terraform files and ensured all required resources. Return back '-turn_off' branches
    [9e1fcd0c-f495-4fc9-b50e-a88f54a1675a] - Update CORS origins in `appsettings.Development.json` for updated Azure static app URL
    
    No changes made; add trailing newline to `vite.config.ts`. for trigger new action for frontend move-terraform-files branch
    
    [9e1fcd0c-f495-4fc9-b50e-a88f54a1675a] - Update GitHub workflows to include `develop-move-terraform-files` branch triggers temporaly
    
    [9e1fcd0c-f495-4fc9-b50e-a88f54a1675a] - Update .NET project to target `net10.0`, remove Terraform state and variable files.

---
+++ Infrastructure/Terraform/providers.tf
@@ -0,0 +1,16 @@
+terraform {
+  # backend "azurerm" {
+  # }
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.azure_subscription_id
+}
